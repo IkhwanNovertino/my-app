58802bb00bce64ddb2b614bbbcfd41f1d4969bd7
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-12-01 23:39:12 +0800

    belajar obtimalisasi image di next.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,81 @@ import Layout from '../component/layout'
 const Home: NextPage = () => {
   return (
       <Layout titlePage="Home Page">
-        <h1 className={styles['title-homepage']}>Welcome Ikhwan</h1>
+        <Image src="/banner01.jpg"width={980} height={270} alt="photo profile"/>
+        <h1 className={styles['title-homepage']}>Pekan Robotics Nasional</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit? 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a.
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <Image src="/post01.jpg" width={500} height={300} alt="post 01"/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit? 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a.
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a.
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a.
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, mollitia a. 
+          Quam quos impedit quisquam similique incidunt voluptas quas vero sed fugit sequi explicabo, alias ad esse nesciunt ipsam velit?</p>
+        <Image src="/profile.jpg" width={60} height={60} alt="profile"/>
+        <span>Ikhwan Novertino, Eight Squad</span>
       </Layout>
   )
 }
