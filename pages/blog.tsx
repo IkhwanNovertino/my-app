2b8f95b512c811708ab1c22398e21ecbf81a0f6d
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-12-01 13:50:28 +0800

    melakukan proses routing

---
+++ pages/blog.tsx
@@ -0,0 +1,9 @@
+// import React from "react"; => tidak perlu mengimport react karena sudah disupport
+
+export default function Blog() {
+    return(
+        <div>
+            <h1>User Blog!!</h1>
+        </div>
+    )
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,12 @@
+
+/**
+ * Halaman ini digunakan jika kita ingin memlakukan routing ke "localhost:3000/users",
+ * karena users berbentuk folder maka harus ada file index didalamnya.
+ */
+export default function Users() {
+    return (
+        <div>
+            <h1>Users Page</h1>
+        </div>
+    )
+}
