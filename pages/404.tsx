74d49c0a7db2f04cc46365fb41faf2c5c8d4585a
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-12-01 22:17:26 +0800

    membuat custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h3 className="title-not-found">Ooops...</h3>
+      <h3 className="title-not-found">404 | Halaman tidak ditemukan</h3>
+    </div>
+  )
+};
