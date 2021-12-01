5c64680b7ff340afd5fcef5d5431060fc0614d61
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-12-01 22:26:47 +0800

    melakukan auto redirections

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+
+
 export default function Custom404() {
+
+  const router = useRouter() //digunakan untuk melakukan auto redirections
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`Dipanggil setelah 5 detik`)
+      router.push('/')
+    }, 5000);
+  }, [])
+
   return (
     <div>
       <h3 className="title-not-found">Ooops...</h3>
