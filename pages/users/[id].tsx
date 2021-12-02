da873512bdd8363613adf80a29f283128f85e65f
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2021-12-02 15:13:32 +0800

    memahami getStaticPaths

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,11 +1,59 @@
-import { useRouter } from "next/router"
+import Layout from "../../component/layout"
 
-export default function UserDetail() {
-  const router = useRouter()
-  const {id} = router.query
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+interface UserDetailProps {
+  user: User;
+}
+
+export default function UserDetail(props: UserDetailProps) {
+  const { user } = props
+
     return (
-        <div>
-          <h2>User Detail Page {id} </h2>
-        </div>
+        <Layout titlePage="User Detail">
+          <ul>
+            <li>{user.name}</li>
+            <li>{user.email}</li>
+            <li>{user.phone}</li>
+            <li>{user.website}</li>
+          </ul>
+        </Layout>
     )
+}
+
+export async function getStaticPaths() {
+  const users = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await users.json();
+  const paths = dataUsers.map( (user : User) => ({
+    params: {
+      id : `${user.id}`
+    }
+  }))
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+interface GetStaticProps {
+  params: {
+    id: string;
+  }
+}
+
+export async function getStaticProps(context: GetStaticProps) {
+  const {id} = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const iduser = await res.json();
+  return {
+    props: {
+      user: iduser,
+    }
+  }
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,4 +1,5 @@
-import Head from "next/head";
+import styles from '../../styles/Users.module.css';
+import { useRouter } from "next/router";
 import Layout from "../../component/layout";
 
 
@@ -6,12 +7,37 @@ import Layout from "../../component/layout";
  * Halaman ini digunakan jika kita ingin memlakukan routing ke "localhost:3000/users",
  * karena users berbentuk folder maka harus ada file index didalamnya.
  */
-export default function Users() {
-  return (
-    
+
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+
+export default function Users(props: UsersProps) {
+  const {dataUsers} = props
+  const router = useRouter()
+  //console.log(dataUsers);
+  
+  return (  
     <Layout titlePage="Users Page">
-      <h1>Users Page</h1>
+      {dataUsers.map(user => {
+        return (
+          <div className={styles.card} key={user.id} onClick={() => router.push(`/users/${user.id}`)}>
+            <p className={styles.item}>{user.name}</p>
+            <p className={styles.item}>{user.email}</p>
+          </div>
+        )
+      })}
     </Layout>
     
   )
+}
+
+export async function getStaticProps() {
+  const users = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await users.json();
+  return {
+    props: {
+      dataUsers,  
+    }
+  }
 }
